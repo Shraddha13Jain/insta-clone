a446fd8345a3edb46bf832c42c62d5a932b01f8a
Shraddha13Jain <shraddhaj829@gmail.com>
2021-05-04 10:50:22 +0530

    added deletepost feature

--- routes/post.js
+++ routes/post.js
@@ -101,4 +101,22 @@ router.put('/comment',requireLogin,(req,res)=>{
 })
 
 
+router.delete('/deletepost/:postid',requireLogin,(req,res)=>{
+    Post.findOne({_id:req.params.postid})
+    .populate("postedBy","_id")
+    .exec((err,post)=>{
+        if(err || !post){
+            return res.status(422).json({error:err})
+        }
+        if(post.postedBy._id.toString() === req.user._id.toString()){
+                post.remove()
+                .then(result=>{
+                    res.json(result)
+                }).catch(err=>{
+                    console.log(err)
+                })
+        }
+    })
+})
+
 module.exports = router
