d73089571feb75999e8b1f508987277df1222b72
Shraddha13Jain <shraddhaj829@gmail.com>
2021-05-05 16:50:19 +0530

    reset password

--- routes/auth.js
+++ routes/auth.js
@@ -117,4 +117,25 @@ router.post('/reset-password',(req,res)=>{
      })
 })
 
+router.post('/new-password',(req,res)=>{
+    const newPassword=req.body.password;
+    const senttoken=req.body.token
+    User.findOne({resetToken:senttoken,expireToken:{$gt:Date.now()}})
+    .then(user=>{
+        if(!user){
+            return res.status(422).json({error:"try again session expired"})
+        }
+        bcrypt.hash(newPassword,12).then(hashedpassword=>{
+               user.password=hashedpassword
+               user.resetToken=undefined
+               user.expireToken=undefined,
+               user.save().then((saveduser)=>{
+                   res.json({message:"password updated success"})
+               })
+        })
+    }).catch(err=>{
+        console.log(err);
+    })
+})
+
 module.exports= router;
